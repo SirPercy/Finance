c76e929454e1c89d84b7d599c2fe93f98fa32a96
Peter <peter@meridium.se>
2014-12-10 21:50:28 +0100

    logger and ret of invest

--- Finance/Core/Jobs/UpdatePortfolioJob.cs
+++ Finance/Core/Jobs/UpdatePortfolioJob.cs
@@ -11,30 +11,39 @@ namespace Finance.Core.Jobs
     {
         public void Execute(IJobExecutionContext context)
         {
-            var repository = new Repository.Repository();
-            var portfolio = repository.GetPortfolio();
-            foreach (var item in portfolio)
+            try
             {
-                var currentItem = item;
-                 var ticker = repository.GetTickers().FirstOrDefault(t => t.FullName.Equals(currentItem.Stock));
-                //post in not one of the stocks in list to follow
-                if (ticker == null)
-                    continue;
+                var repository = new Repository.Repository();
+                var portfolio = repository.GetPortfolio();
+                foreach (var item in portfolio)
+                {
+                    var currentItem = item;
+                    var ticker = repository.GetTickers().FirstOrDefault(t => t.FullName.Equals(currentItem.Stock));
+                    //post in not one of the stocks in list to follow
+                    if (ticker == null)
+                        continue;
 
-                var date = DateTime.Now.AddDays(-1);
-                if (date.DayOfWeek.Equals(DayOfWeek.Sunday))
-                    date = DateTime.Now.AddDays(-3);
-                if (date.DayOfWeek.Equals((DayOfWeek.Saturday)))
-                    date = DateTime.Now.AddDays(-2);
+                    var date = DateTime.Now.AddDays(-1);
+                    if (date.DayOfWeek.Equals(DayOfWeek.Sunday))
+                        date = DateTime.Now.AddDays(-3);
+                    if (date.DayOfWeek.Equals((DayOfWeek.Saturday)))
+                        date = DateTime.Now.AddDays(-2);
 
-                var price = QuoteService.GetPrice(ticker.TickerName, date);
-                if(price.Last == null)
-                    continue;
-                var calcPrice = Double.Parse(price.Last, System.Globalization.CultureInfo.InvariantCulture);
+                    var price = QuoteService.GetPrice(ticker.TickerName, date);
+                    if (price.Last == null)
+                        continue;
+                    var calcPrice = Double.Parse(price.Last, System.Globalization.CultureInfo.InvariantCulture);
 
-                var entity = repository.Context.Portfolio.FirstOrDefault(i => i.Id.Equals(currentItem.Id));
-                entity.CurrentPrice = calcPrice;
-                repository.SaveChanges();
+                    var entity = repository.Context.Portfolio.FirstOrDefault(i => i.Id.Equals(currentItem.Id));
+                    entity.CurrentPrice = calcPrice;
+                    repository.SaveChanges();
+                }
+                Logger.AddMessage("[OK] Updateportfoliojob " + DateTime.Now);
+            }
+            catch(Exception ex)
+            {
+                Logger.AddMessage("[ERROR] Updateportfoliojob " + ex.InnerException);
+ 
             }
         }
     }
