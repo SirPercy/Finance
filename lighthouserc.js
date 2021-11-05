714ea1a999f272d3e19efda4f798a1a089ba6659
Peter Eriksson <xgekko@gmail.com>
2021-11-05 15:24:51 +0100

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,16 +1,14 @@
 module.exports = {
   ci: {
     collect: {
-      numberOfRuns: 3,
-      staticDistDir: "./dist/ng-github-lighthouse-ci",
-    },
-    assert: {
-      assertions: {
-        "first-contentful-paint": ["warn", { maxNumericValue: 4000 }],
-      },
+      startServerCommand: 'npm run server',
+      url: ['http://localhost:4000/'],
+      startServerReadyPattern: 'Server is running on PORT 4000',
+      startServerReadyTimeout: 20000 // milliseconds
+      numberOfRuns: 5,
     },
     upload: {
-      target: "temporary-public-storage",
+      target: 'temporary-public-storage',
     },
   },
 };
